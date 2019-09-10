361f540b371488eba32b44c9bc51a9ab1d2e3fe5
julien-lav <julien.laville@hotmail.fr>
2019-09-10 16:10:37 +0200

    Adding profile :rocket:

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 ### Important Commands
 
+Run th server: 
 ```
 python manage.py runserver
 ```
 
+Migrations:
+```
+python manage.py makemigrations
+```
+```
+python manage.py migrate
+```
+
 ### Helpfull Commands
 ```
- python manage.py startapp user
+ python manage.py shell
+```
+
+```
+ python manage.py startapp resources
+```
+
+To manage images in django:
+```
+pip install pillow
 ```
--- musicSchool/urls.py
+++ musicSchool/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic.base import TemplateView 
 from users import views as user_views
 
@@ -25,4 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),  
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
