4a34cfc723ad80e93b34cbc5832938ab36ef5c55
Nathan <nathan.michel679@gmail.com>
2019-09-10 14:49:01 +0200

    finitions custom graph

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,6 +1,14 @@
+$(document).ready(function() {
+  reset();
+})
+
 document.getElementById('reset-button').addEventListener("click", function(event){
   event.preventDefault();
-  Array.from(document.getElementsByClassName('form-control')).forEach( item => {
-    item.value = null;
-  });
+  reset();
 })
+
+function reset() {
+  Array.from(document.getElementsByClassName('form-control')).forEach( item => {
+    item.value = 0  ;
+  });
+}
