4a992d29a87da7caf72fd1939bb5c83dd34b0b78
PierreMr <mauerpierre@gmail.com>
2019-09-12 12:39:57 +0200

    Resources | Courses :snowman:

--- resources/models.py
+++ resources/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from courses.models import Course
 
 class Resource(models.Model):
     url = models.CharField(max_length=549)
     teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resource_teacher')
-    students = models.ManyToManyField(User, related_name='resource_students')
+    courses = models.ManyToManyField(Course, related_name='resource_courses')
