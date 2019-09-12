0d359c544d1517bd2561ad7b808ff42ac942bdb7
PierreMr <mauerpierre@gmail.com>
2019-09-12 14:58:31 +0200

    :snowman:

--- courses/forms.py
+++ courses/forms.py
@@ -1,9 +1,11 @@
 from django import forms
-from .models import Course
+from .models import Course, DAY_OF_THE_WEEK_CHOICES
 
 class CourseForm(forms.ModelForm):
-    startTime = forms.TimeField(widget=forms.TimeInput(attrs={'placeholder': 'HH:mm'}))
-    endTime = forms.TimeField(widget=forms.TimeInput(attrs={'placeholder': 'HH:mm'}))
+    dayOfTheWeek = forms.ChoiceField(choices = DAY_OF_THE_WEEK_CHOICES)
+    startTime = forms.TimeField(widget=forms.TimeInput(attrs={'placeholder': 'HH:mm', 'type': 'time'}))
+    endTime = forms.TimeField(widget=forms.TimeInput(attrs={'placeholder': 'HH:mm', 'type': 'time'}))
+
     class Meta:
         model = Course
         exclude = ["teacher", "students"]
--- courses/models.py
+++ courses/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+DAY_OF_THE_WEEK_CHOICES = (
+    (0, "Lundi"),
+    (1, "Mardi"),
+    (2, "Mercredi"),
+    (3, "Jeudi"),
+    (4, "Vendredi"),
+    (5, "Samedi"),
+    (6, "DImanche"),
+)
+
 class Course(models.Model):
     topic = models.CharField(max_length=549)
-    dayOfTheWeek = models.PositiveSmallIntegerField()
+    dayOfTheWeek = models.PositiveSmallIntegerField(
+        choices=DAY_OF_THE_WEEK_CHOICES,
+        default=0
+    )
     startTime = models.TimeField()
     endTime = models.TimeField()
     teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='course_teacher')
